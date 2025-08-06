2e18439c04d15a8efd49c4c1af02630a9958821e
ibaialberdi <ibaialberdi@gmail.com>
2025-08-06 12:46:12 +0200

    feat: add card enabled validation middleware

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { createAccount, getAccounts } from "./controllers/account-controller"
 import { changeCardPin, createCard } from "./controllers/card-controller"
 import { createUser, getUsers } from "./controllers/user-controller"
 import { initDB } from "./database/db"
+import { validateCardEnabled } from "./middlewares/cardenabled-middleware"
 import { validatePin } from "./middlewares/pin-middleware"
 
 await initDB()
@@ -16,7 +17,7 @@ app.get("/", (req, res) => {
 })
 
 app.post("/accounts", createAccount)
-app.get("/accounts", getAccounts)
+app.get("/accounts", validatePin, validateCardEnabled, getAccounts)
 app.post("/users", createUser)
 app.get("/users", getUsers)
 app.post("/cards", createCard)
