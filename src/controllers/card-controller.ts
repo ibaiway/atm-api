141853e4df345f29cf0c88c946e6294d1a3f1918
ibaialberdi <ibaialberdi@gmail.com>
2025-08-06 12:21:51 +0200

    feat: integrate bcrypt for secure PIN handling

--- src/controllers/card-controller.ts
+++ src/controllers/card-controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express"
 import { addCard } from "../services/card-service"
+import { generateRandomPIN, hashPIN } from "../utils/pin-utils"
 
 export const createCard = async (req: Request, res: Response) => {
   const number = req.body.number // TODO: this will need to be generated and checked for uniqueness
 
-  const pin = "1111" // TODO: generete random pin and store hashed in db
+  const pin = generateRandomPIN()
+  const hashedPin = await hashPIN(pin)
 
-  await addCard(req.body.accountId, req.body.name, number, null, pin)
+  await addCard(req.body.accountId, req.body.name, number, null, hashedPin)
   res.status(201).json({ number, pin })
 }
